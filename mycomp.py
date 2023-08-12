de824c47808ce65b9c81c6fd8ba5b966687db1c1
paf8128 <110883924+paf8128@users.noreply.github.com>
2023-08-12 17:31:39 +0800

    Update mycomp.py

--- mycomp.py
+++ mycomp.py
@@ -1,3 +1,4 @@
+from math import atan2,sin,cos,pi
 I = 1j
 def c2t(comp:complex):
     return (comp.real,comp.imag)
@@ -7,3 +8,6 @@ def tint(tup:tuple):
     return tuple(map(int,tup))
 def t2c(tup:tuple):
     return complex(tup[0],tup[1])
+#将直角坐标转化为极坐标(向量)
+def rect2polar(comp:complex):
+    return atan2(*rc2t(comp)),(comp.real**2+comp.imag**2)**0.5
