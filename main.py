0a8d245645dc0ea8c65ff3a476098e7c2eb9a941
paf8128 <110883924+paf8128@users.noreply.github.com>
2024-06-06 13:16:31 +0800

    Add files via upload

--- main.py
+++ main.py
@@ -1,16 +1,16 @@
 from mymap import *
 from mycomp import *
 from random import randint,random
-import json
-import bullet,player
+import bullet,player,propreties,time
 pygame.init()
 font = pygame.font.Font("propreties/simkai.ttf",SQSIZE - 40)
+font2 = pygame.font.Font("propreties/simkai.ttf",SQSIZE - 80)
+count = 0
 timer = pygame.time.Clock()
-with open("propreties/propreties.json") as f:
-    data = json.load(f)
+fps = propreties.data["fps"]
 waittime = 0
 gene = make_generator()
-width,height = data["maxcols"]*SQSIZE+WALLWIDTH,(data["maxrows"]+1)*SQSIZE+WALLWIDTH
+width,height = propreties.data["maxcols"]*SQSIZE+WALLWIDTH,(propreties.data["maxrows"]+1)*SQSIZE+WALLWIDTH
 screen = pygame.display.set_mode((width,height))
 p1 = player.Player((255,0,0))
 p2 = player.Player((0,255,0))
@@ -18,7 +18,10 @@ players = (p1,p2)
 keepgoing = True
 pause = False
 effect_bullets = []
+timetest = time.perf_counter()
+real_fps = 60
 while keepgoing:
+    count = (count+1)%60
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             keepgoing = False
@@ -66,19 +69,19 @@ while keepgoing:
                 p2.move_mode = 0
     screen.fill((255,255,255))
     alive_players = []
-    for ply in players:
-        if ply.lives > 0:
-            alive_players.append(ply)
-            ply.update(game_map)
-            ply.draw(screen)
     for b in effect_bullets.copy():
         if b.is_effect():
             b.update(game_map,(p1,p2))
             b.draw(screen)
         else:
             effect_bullets.remove(b)
+    for ply in players:
+        if ply.lives > 0:
+            alive_players.append(ply)
+            ply.update(game_map)
+            ply.draw(screen)
     if len(alive_players) <= 1 and waittime == -1:
-        waittime = 40*3
+        waittime = fps*3
     if waittime > 0:
         waittime -= 1
     if waittime == 0:
@@ -86,17 +89,21 @@ while keepgoing:
         if len(alive_players) == 1:
             alive_players[0].score += 1
         effect_bullets.clear()
-        cols = randint(data["mincols"],data["maxcols"])
-        rows = randint(data["minrows"],data["maxrows"])
-        startx = (data["maxcols"] - cols)*SQSIZE//2
-        starty = (data["maxrows"] - rows)*SQSIZE//2
+        cols = randint(propreties.data["mincols"],propreties.data["maxcols"])
+        rows = randint(propreties.data["minrows"],propreties.data["maxrows"])
+        startx = (propreties.data["maxcols"] - cols)*SQSIZE//2
+        starty = (propreties.data["maxrows"] - rows)*SQSIZE//2
         walls = gene.send((cols,rows))
         game_map = Map(walls,screen,cols,rows,startx,starty)
         p1.newgame(complex(randint(0,cols-1)*SQSIZE+70+startx,randint(0,rows-1)*SQSIZE+70+starty),random()*2*pi)
         p2.newgame(complex(randint(0,cols-1)*SQSIZE+70+startx,randint(0,rows-1)*SQSIZE+70+starty),random()*2*pi)
     screen.blit(font.render(f"红方:{players[0].score}",True,(255,0,0)),(width//8,height-SQSIZE+20))
     screen.blit(font.render(f"绿方:{players[1].score}",True,(0,255,0)),(width*5//8,height-SQSIZE+20))
+    if count == 0:
+        real_fps = 1/(time.perf_counter()-timetest)
+    timetest = time.perf_counter()
+    blit_center(screen,font2.render(f"fps:{int(real_fps)}",True,(0,0,0)),(width//2,height-SQSIZE+20))
     game_map.draw()
-    timer.tick(40)
+    timer.tick(fps)
     pygame.display.update()
 pygame.quit()
